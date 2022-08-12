3274446ecc7fd6abb742ee08cb33feb6917e09a3
往事随风 <499393751@qq.com>
2022-08-12 21:10:19 +0800

    二次修改

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,7 @@ git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
 # git push -f https://github.com/qddidi/qddidi.github.io.git master
-# git push -f git@github.com:zhanghui08/zhanghui.github.io.git main
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhanghui08/my-blogs.git gh-pages
+git push -f git@github.com:zhanghui08/my-blogs.git main:gh-pages
 
